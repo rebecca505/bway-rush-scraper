b3ebd063dc0c394d6b910cf543cfd4a2ff5bb508
rebecca505 <rlaurelfrey@gmail.com>
2025-08-19 17:30:30 -0400

    designed home page

---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import { Box, Button, Flex, Grid } from "@chakra-ui/react";
+
+
+export default function Home() {
+    return (
+        <div>
+            <Box background={"white"} p="5"></Box>
+            <Box background={"#fe5724"} mx="15vh" mb="10" p="5" color="#e7df03">
+                <center>
+                    <p style={{
+                        fontSize: "5vw",
+                        fontFamily: "Open Sans, sans-serif",
+                        fontStyle: "italic",
+                        color: "#e7df03",
+                        WebkitTextStroke: ".2vw black",
+                    }}>
+                        B'WAY RUSH CHECKER
+                    </p>
+                </center>
+            </Box>
+            <text>
+            <Grid templateColumns="repeat(2, 1fr)" gap={10}>
+                <Box background="#f35423" ml="10">hi</Box>
+                <Grid templateRows="repeat(2, 1fr)" gap={10}>
+                    <center>
+                <Button w="125px" fontSize= "17" background="#f35423" color="#e7df03" textDecoration="black" _hover={{ bg: "#fe5724" }}>VIEW SITE</Button>
+                </center>
+                <center>
+                <Button w="125px" fontSize= "17" background="#f35423" color="#e7df03" textDecoration="black" _hover={{ bg: "#fe5724" }}>JOIN SLACK</Button>
+                </center>
+                </Grid>
+            </Grid>
+            </text>
+        </div>
+    );
+}
