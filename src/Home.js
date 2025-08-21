1788aea83e1cf24c88b2b3fb6a49f86e06e09e38
rebecca505 <rlaurelfrey@gmail.com>
2025-08-20 21:18:47 -0400

    update

--- src/Home.js
+++ src/Home.js
@@ -18,18 +18,16 @@ export default function Home() {
     return (
         <div style={{ background: "#e7df03", minHeight: "100vh", margin: 0 }}>
             <Box background={"#e7df03"} p="5"></Box>
-            <Box background={"#fe5724"} mx="15vh" mb="10" p="5" color="#e7df03" borderColor="black" borderWidth="1px">
-                <center>
-                    <p style={{
-                        fontSize: "5vw",
-                        fontFamily: "Open Sans, sans-serif",
-                        fontStyle: "italic",
-                        color: "#e7df03",
-                        WebkitTextStroke: ".2vw black",
-                    }}>
-                        B'WAY RUSH CHECKER
-                    </p>
-                </center>
+            <Box background={"#fe5724"} mx="15vh" mb="10" p="5" color="#e7df03" borderColor="black" borderWidth="1px" textAlign="center">
+                <p style={{
+                    fontSize: "5vw",
+                    fontFamily: "Open Sans, sans-serif",
+                    fontStyle: "italic",
+                    color: "#e7df03",
+                    WebkitTextStroke: ".2vw black",
+                }}>
+                    B'WAY RUSH CHECKER
+                </p>
             </Box>
             <text>
                 <Grid templateColumns="repeat(2, 1fr)" gap={10}>
