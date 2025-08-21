1fb98f6b510a59598a6d0d1c91c88bbdbae76e5a
rebecca505 <rlaurelfrey@gmail.com>
2025-08-20 21:28:23 -0400

    disable button

--- src/Home.js
+++ src/Home.js
@@ -67,7 +67,8 @@ export default function Home() {
                             <a href="https://bwayrush.com/">VIEW SITE</a>
                         </Button>
                         <Button
-                            asChild
+                            disabled
+                            // asChild
                             borderColor="black"
                             w={dynamicWidth}
                             fontSize={dynamicFont}
@@ -75,7 +76,9 @@ export default function Home() {
                             color="#e7df03"
                             _hover={{ bg: "#fe5724", borderWidth: "2px" }}
                             borderWidth=".5px">
-                            <a href="https://notify-me-network.slack.com/archives/C085NHMTFC5">JOIN SLACK</a>
+                            {/* <a href="https://notify-me-network.slack.com/archives/C085NHMTFC5"> */}
+                            JOIN SLACK
+                            {/* </a> */}
                         </Button>
                     </Flex>
                 </Grid>
